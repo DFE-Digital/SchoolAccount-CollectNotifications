b0a6924aaf2754e2610beeeb4f0cdcfd45ca0630
christopherkelly <chris.kelly@education.gov.uk>
2026-09-14 16:19:59 +0100

    Introduce logging

--- SchoolAccount.CollectionNotifications/Services/StatusChangedLegerMonitoringService.cs
+++ SchoolAccount.CollectionNotifications/Services/StatusChangedLegerMonitoringService.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using SchoolAccount.CollectionNotifications.Interfaces;
 using SchoolAccount.CollectionNotifications.Models;
 using SchoolAccount.CollectionNotifications.Stores;
@@ -5,19 +6,28 @@ using SchoolAccount.CollectionNotifications.Stores;
 namespace SchoolAccount.CollectionNotifications.Services;
 
 public class StatusChangedLegerMonitoringService(
+    ILogger<StatusChangedLegerMonitoringService> logger,
     IEnrollmentStore enrollmentStore,
     LastRanService lastRanService,
-    LedgerStore ledgerStore
+    LedgerStore ledgerStore,
+    GovNotifyService govNotifyService
 )
 {
     public async Task<Result> InvokeAsync(CancellationToken cancellationToken = default)
     {
+        logger.LogInformation("Running {class}", nameof(StatusChangedLegerMonitoringService));
+        
         var recipients = await enrollmentStore.ListAsync(cancellationToken);
+        logger.LogInformation("Received {count} of recipients", recipients.Value.Count);
+        
         var lastRan = await lastRanService.GetTimestamp(cancellationToken);
+        logger.LogInformation("Service last run {lastRan}", lastRan.Value);
+        
         var changes = await ledgerStore.GetWhatHasChangedAsync(
             lastRan.Value, 
             recipients.Value.Select(x => x.LAEStab).Distinct().ToList(), 
             cancellationToken);
+        logger.LogInformation("Found {count} of changes", changes.Value.Count);
         
         return Result.Success();
     }
