802137d5413464dbfebd05737db3fde35709e3ec
christopherkelly <chris.kelly@education.gov.uk>
2026-09-16 11:56:16 +0100

    Cleanup needless service extensions

--- SchoolAccount.CollectNotifications/Extensions/ServiceCollectionExtensions.cs
+++ SchoolAccount.CollectNotifications/Extensions/ServiceCollectionExtensions.cs
@@ -20,17 +20,12 @@ public static class ServiceCollectionExtensions
         return services.AddSingleton<IDbConnectionFactory<TDb>>(_ => new DbConnectionFactory<TDb>(connectionString));
     }
 
-    public static IServiceCollection AddDatabase<TDb>(this IServiceCollection services, string identifier,
-        IConfiguration configuration)
+    public static IServiceCollection AddDatabase<TDb>(this IServiceCollection services, IConfiguration configuration)
     {
+        var identifier = typeof(TDb).Name;
         var factory = () => configuration.GetConnectionString(identifier)
                             ?? throw new ArgumentException($"Connection string for {identifier} was not found.");
         return services.AddDatabase<TDb>(factory);
-    }
-
-    public static IServiceCollection AddDatabase<TDb>(this IServiceCollection services, IConfiguration configuration)
-    {
-        return services.AddDatabase<TDb>(typeof(TDb).Name, configuration);
     }
 
     public static IServiceCollection AddEnrollmentStores(this IServiceCollection services, IConfiguration configuration)
