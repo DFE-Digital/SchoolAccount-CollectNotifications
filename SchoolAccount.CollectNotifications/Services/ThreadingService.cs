118026ef78abce578650d4f526067c5644f62801
christopherkelly <chris.kelly@education.gov.uk>
2026-09-21 10:00:22 +0100

    Tweaked logging

--- SchoolAccount.CollectNotifications/Services/ThreadingService.cs
+++ SchoolAccount.CollectNotifications/Services/ThreadingService.cs
@@ -13,12 +13,13 @@ public class ThreadingService(
     public async Task Batch<TSource>(IEnumerable<TSource> items, CancellationToken cancellationToken, Func<TSource, CancellationToken, Task<bool>> func)
     {
         var batches = items.Chunk(threadingOptions.Value.BatchAmount).ToList();
+        logger.LogInformation("Chunking threadable items into blocks of {ValueBatchAmount} which created {BatchesCount} batches", threadingOptions.Value.BatchAmount, batches.Count);
         
         for (var b = 0; b < batches.Count; b++)
         {
             var batch = batches[b];
             
-            logger.LogInformation($"Batch {b + 1}/{batches.Count}: Starting a new batch of {batch.Length} items");
+            logger.LogInformation("Batch {I}/{BatchesCount}: Starting a new batch of {BatchLength} items", b + 1, batches.Count, batch.Length);
 
             var actions = batch.Select(async (source, i) =>
             {
@@ -26,7 +27,7 @@ public class ThreadingService(
                 {
                     if (threadingOptions.Value.ItemWaitAmountInMs > 0)
                     {
-                        logger.LogDebug($"Pausing {i + 1}/{batch.Length}. Pausing for {threadingOptions.Value.ItemWaitAmountInMs * i} milliseconds to respect rate limits...");
+                        logger.LogDebug("Pausing {I}/{BatchLength}. Pausing for {ValueItemWaitAmountInMs} milliseconds to respect rate limits...", i + 1, batch.Length, threadingOptions.Value.ItemWaitAmountInMs * i);
                         await Task.Delay(TimeSpan.FromMilliseconds(threadingOptions.Value.ItemWaitAmountInMs * i), cancellationToken);
                     }
                     
@@ -54,7 +55,7 @@ public class ThreadingService(
 
             if (threadingOptions.Value.BatchWaitAmountInMs > 0)
             {
-                logger.LogDebug($"Batch finished. Pausing for {threadingOptions.Value.BatchWaitAmountInMs} milliseconds to respect rate limits...");
+                logger.LogDebug("Batch finished. Pausing for {ValueBatchWaitAmountInMs} milliseconds to respect rate limits...", threadingOptions.Value.BatchWaitAmountInMs);
                 await Task.Delay(TimeSpan.FromMilliseconds(threadingOptions.Value.BatchWaitAmountInMs), cancellationToken);
             }
         }
